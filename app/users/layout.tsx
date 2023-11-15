2c913b1698894d1c491c7479a2c38f38617e0170
Melisa Ferreyra <ferreyramelisa91@gmail.com>
2023-11-15 10:28:44 -0300

    add conversations

---
+++ app/users/layout.tsx
@@ -0,0 +1,20 @@
+import getUser from "../actions/getUser"
+import Sidebar from "../components/sidebar/Sidebar"
+import UserList from "./components/UserList"
+
+export default async function UsersLayout({
+    children
+} : {
+    children: React.ReactNode
+}) {
+    const users = await getUser()
+    return (
+        // @ts-expect-error Server Component
+        <Sidebar>
+            <div className="h-full">
+                <UserList items={users} />
+                { children }
+            </div>
+        </Sidebar>
+    )
+}
